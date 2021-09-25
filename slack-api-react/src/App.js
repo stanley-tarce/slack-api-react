9299de2c4942eb70232b0c03856587d07513ba56
jerickborela <84577955+jerickborela@users.noreply.github.com>
2021-09-25 20:48:35 +0800

    added login

--- slack-api-react/src/App.js
+++ slack-api-react/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Login from './components/login/Login.js'
 
 function App() {
   return (
     <div className="App">
-
+      <Login></Login>
     </div>
   );
 }
