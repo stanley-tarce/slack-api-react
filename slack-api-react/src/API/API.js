6f823f747c760289a3f0d565081a946a17ad801b
Stanley Tarce <stanleytarce18@gmail.com>
2021-09-25 13:48:41 +0800

    Added API.js

--- slack-api-react/src/API/API.js
+++ slack-api-react/src/API/API.js
@@ -5,54 +5,99 @@ import qs from 'qs'
 const apiHooks = () => {
     let BASE_URL = '206.189.91.54'
     let contentType = { 'Content-Type': 'application/x-www-form-urlencoded' }
-    let accessToken = { 'access-token': 'TTLtXEQUjd1i0BlR6q8zVg' }
     let client = { 'client': 'TdmPB-prCbBtjStYuOeuCg' }
-    const postUserRegistration = (data) => {
+    const postUserRegistration = async (data) => {
         data = qs.stringify(data)
-        return axios({
-            method: 'post',
-            url: `${BASE_URL}/api/v1/auth`,
-            headers: { ...contentType },
-            data: data
-        })
-            .then(response => response.text())
-            .then(response => console.log(response))
-            .catch(error => console.log(error))
+        try {
+            const response = await axios({
+                method: 'post',
+                url: `${BASE_URL}/api/v1/auth`,
+                headers: { ...contentType },
+                data: data
+            })
+            const output = response.data
+            return output
+        } catch (error) {
+            return console.log(error)
+        }
     }
-    const postCreateUserSession = (data) => {
+    const postCreateUserSession = async (data) => {
         data = qs.stringify(data)
-        return axios({
-            method: 'post',
-            url: `${BASE_URL}/api/v1/auth/sign_in`,
-            headers: { ...contentType },
-            data: data
-        })
-            .then(response => response.text())
-            .then(response => console.log(response))
-            .catch(error => console.log(error))
+        try {
+            const response = await axios({
+                method: 'post',
+                url: `${BASE_URL}/api/v1/auth/sign_in`,
+                headers: { ...contentType },
+                data: data
+            })
+            const output = response.data
+            return output
+        } catch (error) {
+            return console.log(error)
+        }
     }
-    const getAllUsers = (expiry, userEmail) => {
-        return axios({
-            method: 'get',
-            url: `${BASE_URL}/api/v1/users`,
-            headers: {
-                ...accessToken,
-                ...client,
-                'expiry': expiry,
-                'uid': userEmail
-            },
-        })
-            .then(response => response.text())
-            .then(response => console.log(response))
-            .catch(error => console.log(error))
+    const getAllUsers = async (expiry, userEmail, accessToken) => {
+        try {
+            const response = await axios({
+                method: 'get',
+                url: `${BASE_URL}/api/v1/users`,
+                headers: {
+                    'access-token': qs.stringify(accessToken),
+                    ...client,
+                    'expiry': qs.stringify(expiry),
+                    'uid': qs.stringify(userEmail)
+                },
+            })
+            const output = response.data
+            return output
+        } catch (error) {
+            return console.log(error)
+        }
     }
-    const getRetrieveAllChannels = () => {
+    const getRetrieveAllChannels = async (expiry, userEmail, accessToken) => {
+        try {
+            const response = await axios({
+                method: 'get',
+                url: `${BASE_URL}/api/v1/channels`,
+                headers: {
+                    'access-token': qs.stringify(accessToken),
+                    ...client,
+                    'expiry': qs.stringify(expiry),
+                    'uid': qs.stringify(userEmail)
+                },
+            })
+            const output = response.data
+            return output
+        } catch (error) {
+            return console.log(error)
+        }
+    }
+    const getRetrieveChannel = async (expiry, userEmail, accessToken) => {
+        try {
+            const response = await axios({
+                method: 'get',
+                url: `${BASE_URL}/api/v1/channels`,
+                headers: {
+                    'access-token': qs.stringify(accessToken),
+                    ...client,
+                    'expiry': qs.stringify(expiry),
+                    'uid': qs.stringify(userEmail)
+                },
+            })
+            const output = response.data
+            return output
+        } catch (error) {
+            return console.log(error)
+        }
+    }
 
-    }
     return {
         postUserRegistration,
         postCreateUserSession,
-        getAllUsers
+        getAllUsers,
+        getRetrieveAllChannels,
+        getRetrieveChannel
+
     }
 }
 
