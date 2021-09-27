2554aa695171f47964a3d12b5356f6fe00b5423e
stanley-tarce <79818058+stanley-tarce@users.noreply.github.com>
2021-09-27 08:37:53 +0800

    Finalized API Calls and will integrate changes once Component has been finalized

--- slack-api-react/src/Components/API/API.js
+++ slack-api-react/src/Components/API/API.js
@@ -2,23 +2,27 @@ import axios from 'axios'
 import qs from 'qs'
 
 
+
+
 const apiHooks = () => {
+
+    /* Register User */
     let BASE_URL = '206.189.91.54'
     let contentType = { 'Content-Type': 'application/x-www-form-urlencoded' }
-    let client = { 'client': 'TdmPB-prCbBtjStYuOeuCg' }
-    const postUserRegistration = async (data) => {
+    const postUserRegistration = async (dataRef) => {
         try {
             const response = await axios({
                 method: 'POST',
                 url: `http://${BASE_URL}/api/v1/auth`,
                 headers: { ...contentType },
-                data: qs.stringify(data)
+                data: qs.stringify(dataRef)
             })
             return console.log(response.data)
         } catch (error) {
             return console.log(error)
         }
     }
+
     const postCreateUserSession = async (dataRef) => {
         try {
             const response = await axios({
@@ -42,67 +46,194 @@ const apiHooks = () => {
             return console.log(error)
         }
     }
-    const getAllUsers = async (expiry, userEmail, accessToken) => {
+    const getAllUsers = async (headers) => {
+        const { expiry, uid, accessToken, client } = headers
         try {
             const response = await axios({
                 method: 'get',
-                url: `http://${BASE_URL}/api/v1/auth`,
+                url: `http://${BASE_URL}/api/v1/users`,
                 headers: {
                     'access-token': qs.stringify(accessToken),
-                    ...client,
+                    'client': qs.stringify(client),
                     'expiry': qs.stringify(expiry),
-                    'uid': qs.stringify(userEmail)
+                    'uid': qs.stringify(uid)
                 },
             })
-            const output = response.data
-            return output
+            return console.log(response)
         } catch (error) {
             return console.log(error)
         }
     }
-    const getRetrieveAllChannels = async (expiry, userEmail, accessToken) => {
+    const getRetrieveAllChannels = async (headers) => {
+        const { expiry, uid, accessToken, client } = headers
         try {
             const response = await axios({
                 method: 'get',
-                url: `http://${BASE_URL}/api/v1/auth`,
+                url: `http://${BASE_URL}/api/v1/channels`,
                 headers: {
                     'access-token': qs.stringify(accessToken),
-                    ...client,
+                    'client': qs.stringify(client),
                     'expiry': qs.stringify(expiry),
-                    'uid': qs.stringify(userEmail)
+                    'uid': qs.stringify(uid)
                 },
             })
-            const output = response.data
-            return output
+            return console.log(response.data)
         } catch (error) {
             return console.log(error)
         }
     }
-    const getRetrieveChannel = async (expiry, userEmail, accessToken) => {
+    const getRetrieveChannel = async (headers, CHANNEL_ID) => {
+        const { expiry, uid, accessToken, client } = headers
         try {
             const response = await axios({
                 method: 'get',
-                url: `http://${BASE_URL}/api/v1/auth`,
+                url: `http://${BASE_URL}/api/v1/channels/${CHANNEL_ID}`,
                 headers: {
                     'access-token': qs.stringify(accessToken),
-                    ...client,
+                    'client': qs.stringify(client),
                     'expiry': qs.stringify(expiry),
-                    'uid': qs.stringify(userEmail)
+                    'uid': qs.stringify(uid)
                 },
             })
-            const output = response.data
-            return output
+            return console.log(response.data)
         } catch (error) {
             return console.log(error)
+        }
+    }
+    const postInviteUserToChannel = async (headers, dataRef) => {
+        //Data -> Channel_ID (id) & member_id
+        const { expiry, uid, accessToken, client } = headers
+        try {
+            const response = await axios({
+                method: 'post',
+                url: `http://${BASE_URL}/api/v1/channel/$add_member`,
+                headers: {
+                    'access-token': qs.stringify(accessToken),
+                    'client': qs.stringify(client),
+                    'expiry': qs.stringify(expiry),
+                    'uid': qs.stringify(uid)
+                },
+                data: qs.stringify(dataRef)
+            })
+            return console.log(response)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    const postCreateChannelWithUsers = async (headers, dataRef) => {
+        //Data -> Channel_Name & user_ids[]
+        const { expiry, uid, accessToken, client } = headers
+        try {
+            const response = await axios({
+                method: 'post',
+                url: `http://${BASE_URL}/api/v1/channels`,
+                headers: {
+                    'access-token': qs.stringify(accessToken),
+                    'client': qs.stringify(client),
+                    'expiry': qs.stringify(expiry),
+                    'uid': qs.stringify(uid)
+                },
+                data: qs.stringify(dataRef)
+            })
+            return console.log(response)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    const getRetrieveAllMessagesInsideChannel = async (headers, CHANNEL_ID, RECEIVER_CLASS) => {
+        const { expiry, uid, accessToken, client } = headers
+        try {
+            const response = await axios({
+                method: 'get',
+                url: `http://${BASE_URL}/api/v1/messages?receiver_id=${CHANNEL_ID}&receiver_class=Channel`,
+                headers: {
+                    'access-token': qs.stringify(accessToken),
+                    'client': qs.stringify(client),
+                    'expiry': qs.stringify(expiry),
+                    'uid': qs.stringify(uid)
+                },
+            })
+            return console.log(response.data)
+        } catch (error) {
+            return console.log(error)
+        }
+    }
+    const getRetrieveAllMessagesFromUser = async (headers, USER_ID) => {
+        const { expiry, uid, accessToken, client } = headers
+        try {
+            const response = await axios({
+                method: 'get',
+                url: `http://${BASE_URL}/api/v1/messages?receiver_id=${USER_ID}&receiver_class=User`,
+                headers: {
+                    'access-token': qs.stringify(accessToken),
+                    'client': qs.stringify(client),
+                    'expiry': qs.stringify(expiry),
+                    'uid': qs.stringify(uid)
+                },
+            })
+            return console.log(response.data)
+        } catch (error) {
+            return console.log(error)
+        }
+    }
+    const postCreateMessageInAChannel = async (headers, dataRef) => {
+        const { expiry, uid, accessToken, client } = headers
+        try {
+            const response = await axios({
+                method: 'post',
+                url: `http://${BASE_URL}/api/v1/messages`,
+                headers: {
+                    'access-token': qs.stringify(accessToken),
+                    'client': qs.stringify(client),
+                    'expiry': qs.stringify(expiry),
+                    'uid': qs.stringify(uid)
+                },
+                data: qs.stringify(dataRef)
+            })
+            return console.log(response)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    const postCreateDirectMessagetoUser = async (headers, dataRef) => {
+        const { expiry, uid, accessToken, client } = headers
+        try {
+            const response = await axios({
+                method: 'post',
+                url: `http://${BASE_URL}/api/v1/messages`,
+                headers: {
+                    'access-token': qs.stringify(accessToken),
+                    'client': qs.stringify(client),
+                    'expiry': qs.stringify(expiry),
+                    'uid': qs.stringify(uid)
+                },
+                data: qs.stringify(dataRef)
+            })
+            return console.log(response)
+        }
+        catch (error) {
+            console.log(error)
         }
     }
 
     return {
+        //Registration
         postUserRegistration,
         postCreateUserSession,
         getAllUsers,
+        //Channels
         getRetrieveAllChannels,
-        getRetrieveChannel
+        getRetrieveChannel,
+        postInviteUserToChannel,
+        postCreateChannelWithUsers,
+        //Messages
+        getRetrieveAllMessagesInsideChannel,
+        getRetrieveAllMessagesFromUser,
+        postCreateMessageInAChannel,
+        postCreateDirectMessagetoUser
     }
 }
 
