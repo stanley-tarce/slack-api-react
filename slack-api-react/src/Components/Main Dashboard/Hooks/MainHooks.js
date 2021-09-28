62ebb576d38506ba5b79f88746e7c2c084c312b6
Stanley Tarce <stanleytarce18@gmail.com>
2021-09-28 12:50:05 +0800

    Created Sidebar
    Hello groupmates, I have created the sidebar but it's not fully functional yet. Currently working on stuff for other API since we need more Modals especially on creating a message
    
    Co-Authored-By: Nikki Sabenicio <83851176+nikkiirra@users.noreply.github.com>
    Co-Authored-By: jerickborela <84577955+jerickborela@users.noreply.github.com>

---
+++ slack-api-react/src/Components/Main Dashboard/Hooks/MainHooks.js
@@ -0,0 +1,6 @@
+import { useState } from "react";
+/**
+ * * CREATE A HOOKS API CONTAINER WITH THE FOLLOWING ITEMS
+ * ! 1. API for Channel Lists
+ * ! 2.
+ */
--- slack-api-react/src/Components/Main Dashboard/Sidebar/Sidebar.js
+++ slack-api-react/src/Components/Main Dashboard/Sidebar/Sidebar.js
@@ -3,8 +3,44 @@ import './Sidebar.css'
 function Sidebar() {
 	return (
 		<div className={"Main-Sidebar"}>
-			Hello
-
+			<div className={'SideBar-Title-Name'}>
+				Avion School
+			</div>
+			<div className={'SideBar-Channel-Container'}>
+				<div className={'SideBar-Channel-Title'}>
+					<div></div>
+					<p>Channel</p>
+				</div>
+				<div className={'SideBar-Channel-Lists'}>
+					<p>Stanley</p>
+					{/** 
+					 1. Create a card for mapping channel lists with left margin of 10 px 
+					 2. Map the channel list with the API Data  */}
+				</div>
+				<div className={'SideBar-Channel-CreateChannel'}>
+					<div></div>
+					<p>Add Channels	</p>
+				</div>
+			</div>
+			<div className={'SideBar-Message-Container'}>
+				<div className={'SideBar-Message-Title'}>
+					<div></div>
+					<p>Message</p>
+				</div>
+				<div className={'SideBar-Message-Lists'}>
+					<p>Stanley</p>
+					{/** 
+					 1. Create a card for mapping channel lists with left margin of 10 px 
+					 2. Map the channel list with the API Data  */}
+				</div>
+				<div className={'SideBar-Message-CreateMessage'}>
+					<div></div>
+					<p>Send Message </p>
+				</div>
+			</div>
+			<div className={''}>
+				Avion School
+			</div>
 		</div>
 	)
 }
