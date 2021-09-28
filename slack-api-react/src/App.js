f6c45f6d68ddf504d0032cc15cd3ee1180841c4a
Stanley Tarce <stanleytarce18@gmail.com>
2021-09-28 08:54:07 +0800

    Added Sidebar

--- slack-api-react/src/App.js
+++ slack-api-react/src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
 import Login from './Components/Login/Login';
+import Sidebar from './Components/Main Dashboard/Sidebar/Sidebar';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Sidebar />
     </div>
   );
 }
---
+++ slack-api-react/src/Components/Main Dashboard/Sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar.css'
+function Sidebar() {
+	return (
+		<div className={"Main-Sidebar"}>
+			Hello
+
+		</div>
+	)
+}
+
+export default Sidebar
