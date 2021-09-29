ac29dfe5dc38c0c1c0adcc6d569cfb41ac7c837a
Nikki Sabenicio <nikkiirra@gmail.com>
2021-09-29 21:29:58 +0800

    Created message input box

--- slack-api-react/src/Components/Main Dashboard/Message Box/MessageBox.js
+++ slack-api-react/src/Components/Main Dashboard/Message Box/MessageBox.js
@@ -2,25 +2,15 @@ import React from 'react'
 import './design.css'
 function MessageBox() {
     return (
-<<<<<<< HEAD
-        <div className={"Message-Box"}> 
-        <div className={"Avatar-and-Image"}> 
-            <p className={"User-Name"}>Anonymous</p>
-            <div className={"Image-Avatar"}/> 
-        </div>
-          <p className={"Message-Body"}>Penge ako lumpia</p>  
-               
-        </div>
-=======
-        <div className={"Message-Box"}>
-            <div className={"Avatar-and-Image"}>
-                <p className={"User-Name"}>Anonymous</p>
-                <div className={"Image-Avatar"} />
-            </div>
-            <p className={"Message-Body"}>Penge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpia</p>
+            <div className={"Message-Box"}>
+                <div className={"Avatar-and-Image"}>
+                    <p className={"User-Name"}>Anonymous</p>
+                    <div className={"Image-Avatar"} />
+                </div>
+                <p className={"Message-Body"}>Penge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpiaPenge ako lumpia</p>
 
->>>>>>> 53198a9cb7b43b66ff9bbd49c125497fb9a97fb8
-        </div>
+            </div>
+            
     )
 }
 export default MessageBox
--- slack-api-react/src/Components/Main Dashboard/MessageContainer/MessageContainer.js
+++ slack-api-react/src/Components/Main Dashboard/MessageContainer/MessageContainer.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import './MessageContainer.css'
 import MessageBox from '../Message Box/MessageBox'
+import MessageInput from '../MessageInput/MessageInput'
 function MessageContainer() {
     return (
-        <div className={"Message-Container"}>
-            <MessageBox />
-            <MessageBox />
-            <MessageBox />
-            <MessageBox />
-            <MessageBox />
-        </div>
+            <div className={"Message-Container"}>
+                <MessageBox />
+                <MessageBox />
+                <MessageBox />
+                <MessageBox />
+                <MessageBox />
+                <MessageInput />
+            </div>
+       
     )
+
 }
 
 export default MessageContainer
